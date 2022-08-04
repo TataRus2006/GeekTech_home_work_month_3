2e8d7fe84364aa39ccc90592e45f7242c8044c6f
rustam <Rusik2006@gmail.com>
2022-08-04 13:32:20 +0600

    webhook

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ STORAGE = MemoryStorage()
 TOKEN = config('TOKEN')
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=STORAGE)
-ADMIN = [863372886, 1746047370]
+ADMIN = [863372886, 1746047370, 5367214519]     # -> Rustam, Nuria, Esen
+URL = "https://rustam-bot.herokuapp.com/"
+PORT = config('PORT')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from decouple import config
+
 from aiogram.utils import executor
-from config import dp
+from config import dp, URL, bot
 import logging
 import asyncio
 
@@ -8,8 +10,13 @@ from database.bot_db import sql_create
 
 
 async def on_startup(_):
+    await bot.set_webhook(URL)
     asyncio.create_task(notification.scheduler())
     sql_create()
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
 
 client.register_handlers_client(dp)
@@ -25,4 +32,13 @@ extra.register_handlers_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path="",
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host='0.0.0.0',
+        port=config("PORT", cast=int)
+    )
